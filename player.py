b8c4a072de8ea0954de74c4330574a587e16e657
Grzegorz121979 <pociechagrzegorz@outlook.com>
2024-04-13 18:44:02 +0200

    new class GeniusComputrPlayer

--- player.py
+++ player.py
@@ -18,7 +18,7 @@ class RandomComputerPlayer(Player):
 
 class HumanPlayer(Player):
 
-    def get_move(self, game):
+    def get_move(self, game) -> int:
         valid_square = False
         val = None
         while not valid_square:
@@ -31,3 +31,25 @@ class HumanPlayer(Player):
             except ValueError:
                 print('Invalid square. Try again.')
         return val
+
+
+class GeniusComputerPlayer(Player):
+
+    def get_move(self, game):
+        if len(game.available_move() == 9):
+            square = random.choice(game.available_move())
+        else:
+            square = self.minimax(game, self.letter)
+        return square
+
+    def minimax(self, state, player):
+        max_player = self.letter
+        other_player = 'O' if player == 'X' else 'X'
+
+        if state.current_winner == other_player:
+            return {'position': None,
+                    'score': 1 * (state.num_empty_square() + 1) if other_player == max_player else -1 * (
+                            state.num_empty_square() + 1)
+                    }
+        elif not state.empty_squares():
+            return {'position': None, 'score': 0}
